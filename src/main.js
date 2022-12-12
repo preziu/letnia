b54802549a702149109859b3f31876d7b1a5fa7b
Przemek Ferkaluk <pferkaluk@gmail.com>
2022-12-12 21:24:27 +0100

    imports

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import 'bootstrap';
+import jQuery from 'jquery';
+import 'popper.js';
 import './assets/app.scss'
+
+window.$ = window.jQuery = jQuery
 
 createApp(App).mount('#app');
