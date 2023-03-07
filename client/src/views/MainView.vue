f408a35aebb70ef9a26366e67bfef767283a3b0b
Przemek Ferkaluk <pferkaluk@gmail.com>
2023-03-07 23:38:00 +0100

    add year to main

--- client/src/views/MainView.vue
+++ client/src/views/MainView.vue
@@ -1,36 +1,40 @@
 <template>
     <div class="container">
-        <div class="row d-flex justify-content-center">
-            <img alt="Letnia logo" class="logo" src="@/assets/img/3d2.gif" />
-        </div>
-    
+        <section class="section-sm">
+            <div class="row d-flex justify-content-center">
+                <img alt="Letnia logo" class="logo top position-absolute" src="@/assets/img/3d2.gif" />
+                <img alt="2023" class="year position-relative top w-50" src="@/assets/img/2023.png" />
+            </div>
+        </section>
 
-        <div class="row menu">
-            <div class="col-sm-12 col-md-3">
-                <router-link to="/booking">
-                    <img src="@/assets/img/DiskDrive.png"/>
-                    <div class="text-primary">Zarezerwuj wczasy</div>
-                </router-link>
+        <section class="section-sm fixed-bottom menu">
+            <div class="row">
+                <div class="col-sm-12 col-md-3">
+                    <router-link to="/booking">
+                        <img src="@/assets/img/DiskDrive.png"/>
+                        <div class="text-primary">Zarezerwuj wczasy</div>
+                    </router-link>
+                </div>
+                <div class="col-sm-12 col-md-3">
+                    <router-link to="/timetable">
+                        <img src="@/assets/img/Volume.png" alt="" />
+                        <div class="text-primary">Plan zajec</div>
+                    </router-link>
+                </div>
+                <div class="col-sm-12 col-md-3">
+                    <router-link to="/stay">
+                        <img src="@/assets/img/Internet.png" alt="" />
+                        <div class="text-primary">Osrodek wypoczynkowy</div>
+                    </router-link>
+                </div>
+                <div class="col-sm-12 col-md-3">
+                    <router-link to="/merch">
+                        <img src="@/assets/img/Paint.png" alt="" />
+                        <div class="text-primary">Kup akcesoria</div>
+                    </router-link>
+                </div>
             </div>
-            <div class="col-sm-12 col-md-3">
-                <router-link to="/timetable">
-                    <img src="@/assets/img/Volume.png" alt="" />
-                    <div class="text-primary">Plan zajec</div>
-                </router-link>
-            </div>
-            <div class="col-sm-12 col-md-3">
-                <router-link to="/stay">
-                    <img src="@/assets/img/Internet.png" alt="" />
-                    <div class="text-primary">Osrodek wypoczynkowy</div>
-                </router-link>
-            </div>
-            <div class="col-sm-12 col-md-3">
-                <router-link to="/merch">
-                    <img src="@/assets/img/Paint.png" alt="" />
-                    <div class="text-primary">Kup akcesoria</div>
-                </router-link>
-            </div>
-        </div>
+        </section>
     </div>
 </template>
 
@@ -51,6 +55,14 @@
 .logo {
     max-height: 80vh;
     max-width: 80%;
+}
+
+.year {
+    z-index: -1;
+}
+
+.menu {
+    margin-bottom: 10em;
 }
 
 a {
