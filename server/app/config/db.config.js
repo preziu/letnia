81e3f4fce82b8534e36caa9dd8eb3dbf19035763
Przemek Ferkaluk <pferkaluk@gmail.com>
2023-04-08 17:31:40 +0200

    require dotnev in db.config

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV === "production") {
+    require("dotenv").config();
+    console.log(process.env + " db.config");
+    console.log(process.env.MONGODB_URL + " db.config");
+}
+
 module.exports = {
     url: process.env.MONGODB_URL
     };
