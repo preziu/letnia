1a929fd7fb9566f1e48c59554eb04720f881341a
Przemek Ferkaluk <pferkaluk@gmail.com>
2023-04-08 17:46:22 +0200

    cllean db config

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,9 +1,3 @@
-if (process.env.NODE_ENV === "production") {
-    require("dotenv").config();
-    console.log(process.env + " db.config");
-    console.log(process.env.MONGODB_URL + " db.config");
-}
-
 module.exports = {
     url: process.env.MONGODB_URL
     };
