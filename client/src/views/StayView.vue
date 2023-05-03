34cb122794ae1139fab92a2d61821e3713a01d2d
Przemek Ferkaluk <pferkaluk@gmail.com>
2023-05-03 11:25:43 +0200

    add gallery to stay view

--- client/src/views/StayView.vue
+++ client/src/views/StayView.vue
@@ -11,6 +11,62 @@
         <p>🏨 Ośrodek mieści się w dawnych zabudowaniach klasztornych i gospodarskich zakonu żeńskiego wzniesionego w latach 1765 – 1815. Piaskowiec z XVIII wieku przeplata się tu z modernistyczną wizją PRLu, a wszystko doprowadzone jest do współczesnych standardów 🚀</p>
         <p>❤️ Wspaniałe miejsce z dobrą energią - dla amatorów aktywnego spędzania czasu i miłośników miejsc ustronnych, z duszą ❤️</p>
     </div>
+    <div class="card card-secondary">
+        <div class="card-header text-center pt-3">
+            <h6>Kiedyś to było</h6>
+        </div>
+        <div class="card-body">
+            <div id="carousel" class="carousel slide carousel-fade z-depth-1-half" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="2"></li>
+                    <li data-target="#carousel" data-slide-to="3"></li>
+                    <li data-target="#carousel" data-slide-to="4"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="carousel-item active">
+                        <div class="view">
+                            <img class="d-block w-100" src="@/assets/img/gallery/7.jpg">
+                            <div class="mask rgba-black-light"></div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="view">
+                            <img class="d-block w-100" src="@/assets/img/gallery/8.jpg">
+                            <div class="mask rgba-black-light"></div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="view">
+                            <img class="d-block w-100" src="@/assets/img/gallery/9.jpg">
+                            <div class="mask rgba-black-light"></div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="view">
+                            <img class="d-block w-100" src="@/assets/img/gallery/10.jpg">
+                            <div class="mask rgba-black-light"></div>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <div class="view">
+                            <img class="d-block w-100" src="@/assets/img/gallery/11.jpg">
+                            <div class="mask rgba-black-light"></div>
+                        </div>
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Poprzednie</span>
+                </a>
+                <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Następne</span>
+                </a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style>
