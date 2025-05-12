efcdd33e5f499e3198df94237133313323c9e205
Przemek Ferkaluk <pferkaluk@gmail.com>
2025-05-12 23:26:23 +0200

    refector structure

--- server/server.js
+++ server/server.js
@@ -1,55 +1,48 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
 const app = express();
-app.use(express.json());
 
-var corsOptions = {
-    origin: "http://localhost:8080"
+const corsOptions = {
+    origin:
+        process.env.NODE_ENV === 'production'
+            ? process.env.CLIENT_URL
+            : 'http://localhost:8080'
 };
-
 app.use(cors(corsOptions));
-
-// parse requests of content-type - application/json
+app.use(express.json());
 app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require("./app/routes/booking.routes")(app);
-require("./app/routes/order.routes")(app);
-
-// handle production
+require('./app/routes/booking.routes')(app);
+require('./app/routes/order.routes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    //static folder
-    app.use(express.static(__dirname + '/public/'));
-
-    //handle spa
-    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+    app.use(express.static(path.join(__dirname, 'public')));
+    app.get('/*', (req, res) =>
+        res.sendFile(path.join(__dirname, 'public', 'index.html'))
+    );
 }
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
 
-const db = require("./app/models");
+const db = require('./app/models');
 db.mongoose
     .connect(db.url, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(() => {
-    console.log("Connected to the database!");
-    })
+    .then(() => console.log('Connected to the database!'))
     .catch(err => {
-        console.log("Cannot connect to the database!", err);
+        console.error('Cannot connect to the database!', err);
         process.exit();
     });
