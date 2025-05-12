384665c02a11b8cf6994dfb745c14a56ce950ad4
Przemek Ferkaluk <pferkaluk@gmail.com>
2025-05-12 22:28:06 +0200

    build

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,16 +1,14 @@
 const path = require('path');
 
 module.exports = {
-  transpileDependencies: true,
-
   outputDir: path.resolve(__dirname, '../public'),
-
   devServer: {
     proxy: {
       '/api': {
         target: 'http://localhost:8081',
-        changeOrigin: true,
+        changeOrigin: true
       }
     }
-  }
+  },
+  lintOnSave: false
 };
